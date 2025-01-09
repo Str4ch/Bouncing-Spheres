87fdee3aa691e19f51c6577f57a9134ca86b5509
Str4ch <dog.fek54@gmail.com>
2025-01-09 15:30:25 +0100

    Antialiasing

--- color.hpp
+++ color.hpp
@@ -2,7 +2,8 @@
 #define COLOR_H
 
 #include "vec3.hpp"
-
+#include "interval.hpp"
+#include <string>
 using color = vec3;
 
 std::string write_color(const color& pixel_color) {
@@ -11,9 +12,10 @@ std::string write_color(const color& pixel_color) {
     auto b = pixel_color.z();
 
     // Translate the [0,1] component values to the byte range [0,255].
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     // Write out the pixel color components.
     return std::to_string(rbyte) + ' ' + std::to_string(gbyte) + ' ' + std::to_string(bbyte) + '\n';
